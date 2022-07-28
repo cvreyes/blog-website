46006c6b850d711d3ec664524ee1c0838692526f
Caesar Reyes <caesar.reyes@citystyle.co.jp>
2022-07-28 15:50:57 +0800

    Finish Until Challenge 9

--- app.js
+++ app.js
@@ -15,11 +15,26 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+  res.render("home", {startingContent: homeStartingContent});
+});
 
+app.get("/about", function (req, res) {
+  res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function (req, res) {
+  res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
-
+app.post("/compose", function(req, res) {
+  const blog = req.body.postTitle;
+  console.log(blog)
+})
 
 
 
